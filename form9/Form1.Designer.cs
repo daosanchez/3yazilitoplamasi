7d494b6b29051181834a3a2912991ceb26c16c56
ravza <145916188+ravzasanchez@users.noreply.github.com>
2023-10-02 13:50:49 +0300

    Add files via upload

---
+++ form9/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace form9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.txtsinav1 = new System.Windows.Forms.TextBox();
+            this.txtsinav2 = new System.Windows.Forms.TextBox();
+            this.txtsinav3 = new System.Windows.Forms.TextBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(33, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1sınav";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(33, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "2sınav";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(33, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "3sınav";
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuc.Location = new System.Drawing.Point(32, 157);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(70, 24);
+            this.lblSonuc.TabIndex = 3;
+            this.lblSonuc.Text = "Sonuc";
+            // 
+            // txtsinav1
+            // 
+            this.txtsinav1.Location = new System.Drawing.Point(117, 24);
+            this.txtsinav1.Name = "txtsinav1";
+            this.txtsinav1.Size = new System.Drawing.Size(100, 20);
+            this.txtsinav1.TabIndex = 4;
+            this.txtsinav1.TextChanged += new System.EventHandler(this.txtsinav1_TextChanged);
+            // 
+            // txtsinav2
+            // 
+            this.txtsinav2.Location = new System.Drawing.Point(117, 57);
+            this.txtsinav2.Name = "txtsinav2";
+            this.txtsinav2.Size = new System.Drawing.Size(100, 20);
+            this.txtsinav2.TabIndex = 5;
+            // 
+            // txtsinav3
+            // 
+            this.txtsinav3.Location = new System.Drawing.Point(117, 93);
+            this.txtsinav3.Name = "txtsinav3";
+            this.txtsinav3.Size = new System.Drawing.Size(100, 20);
+            this.txtsinav3.TabIndex = 6;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHesapla.Location = new System.Drawing.Point(117, 208);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(100, 41);
+            this.btnHesapla.TabIndex = 7;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(358, 301);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.txtsinav3);
+            this.Controls.Add(this.txtsinav2);
+            this.Controls.Add(this.txtsinav1);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.TextBox txtsinav1;
+        private System.Windows.Forms.TextBox txtsinav2;
+        private System.Windows.Forms.TextBox txtsinav3;
+        private System.Windows.Forms.Button btnHesapla;
+    }
+}
+
---
+++ form9/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            int Sinav1 = Convert.ToInt32(txtsinav1.Text);
+            int Sinav2 = Convert.ToInt32(txtsinav2.Text);
+            int Sinav3 = Convert.ToInt32(txtsinav3.Text);
+            int ortalama = (Sinav1 + Sinav2 + Sinav3) / 3;
+            lblSonuc.Text = "Sonuc: " + ortalama.ToString();
+        }
+
+        private void txtsinav1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
